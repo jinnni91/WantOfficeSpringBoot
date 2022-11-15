09bf96fee789756b6ef0c10f05281651559775f0
SU JIN KIM <rosysnown@gmail.com>
2022-11-15 17:18:35 +0900

    base commit

---
+++ src/main/java/com/project/office/WantOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.project.office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantOfficeApplication.class, args);
+	}
+
+}
