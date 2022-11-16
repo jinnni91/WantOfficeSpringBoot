4d8ea536fc7a45f209c83cbe3a11219e86688e8f
김현진 <jinnni919191@gmail.com>
2022-11-16 17:48:16 +0900

    [김현진] attendance와 rest controller 생성

---
+++ src/main/java/com/project/office/attendance/controller/AttendanceController.java
@@ -0,0 +1,5 @@
+package com.project.office.attendance.controller;
+
+public class AttendanceController {
+
+}
---
+++ src/main/java/com/project/office/rest/controller/RestController.java
@@ -0,0 +1,5 @@
+package com.project.office.rest.controller;
+
+public class RestController {
+
+}
