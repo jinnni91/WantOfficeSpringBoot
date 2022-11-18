ce62a44979913a43c3d4709f56a73016cb8d2a3c
김승환 <kshlanos@gmail.com>
2022-11-18 17:56:10 +0900

    [김승환] NoticeController, NoticeService 작업중.

--- src/main/java/com/project/office/notice/controller/NoticeController.java
+++ src/main/java/com/project/office/notice/controller/NoticeController.java
@@ -1,5 +1,54 @@
-package com.project.office.notice.controller;
-
-public class NoticeController {
-
-}
+//package com.project.office.notice.controller;
+//
+//import org.springframework.data.domain.Page;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.project.office.common.ResponseDTO;
+//import com.project.office.common.paging.PagingButton;
+//import com.project.office.common.paging.ResponseDTOWithPaging;
+//import com.project.office.common.paging.pagenation;
+//import com.project.office.notice.dto.NoticeDTO;
+//import com.project.office.notice.service.NoticeService;
+//
+//import lombok.extern.slf4j.Slf4j;
+//
+//@Slf4j
+//@RestController
+//@RequestMapping("/api/notice")
+//public class NoticeController {
+//	
+//	private final NoticeService noticeService;
+//	
+//	public NoticeController(NoticeService noticeService) {
+//		this.noticeService = noticeService;
+//	}
+//	
+//	@GetMapping("/list")
+//	public ResponseEntity<ResponseDTO> selectNoticeList(@RequestParam(name="page", defaultValue="1") int page) {
+//		
+//		log.info("[NoticeController] selectNoticeList Start =========");
+//		log.info("[NoticeCOntroller] page : {}", page);
+//		
+//		Page<NoticeDTO> NoticeDtoList = NoticeService.selectNoticeList(page);
+//		
+//		PagingButton pageInfo = pagenation.getPagingButton(NoticeDtoList);
+//		
+//		log.info("[ProductController] pageInfo : {}", pageInfo);
+//		
+//		ResponseDTOWithPaging responseDtoWithPaging = new ResponseDTOWithPaging();
+//		responseDtoWithPaging.setPageBtn(pageInfo);
+//		responseDtoWithPaging.setData(NoticeDtoList.getContent());
+//		
+//		log.info("[NoticeController] selectNoticeList End=====");
+//		
+//		return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "조회 성공", responseDtoWithPaging));
+//		
+//		
+//	}
+//	
+//}
